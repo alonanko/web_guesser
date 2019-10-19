360ca5f53ed8f6b6dc89825f45debc535d01667a
alon <alon.ankonina@gmail.com>
2019-10-18 17:40:29 -0700

    html template added

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-num = rand(100)
+number = rand(100)
 get '/' do
-  "The Secret NumBBer is #{num}"
+  erb :index, :locals => {:number => number}
+
 end
