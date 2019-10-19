89f34fe66dd98b7fac08c43fb0949490a22ec573
alon <alon.ankonina@gmail.com>
2019-10-18 18:16:19 -0700

    compelted game

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
-get '/' do
-  erb :index, :locals => {:number => number}
+NUMBER = rand(100)
 
+def check_guess(guess)
+  guess = guess.to_i
+  if guess > NUMBER
+    message = "too high"
+  elsif guess < NUMBER
+    message = "too low"
+  else
+    message = "The Secrect Number is #{NUMBER}"
+  end
+  return message
+end
+
+get '/' do
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:message => message}
 end
